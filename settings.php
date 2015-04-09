8e8552a49f3c1f5697b1a7f3963dac7be94dbd0c
anes89cok <anes89cok@gmail.com>
2015-04-09 14:14:54 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,24 @@
+
+<?php
+$adresa[0]="1431 Rail Head Blvd #2"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="Naples, Florida 34110";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="Naples";
+$telephone[0]=2392892422;// Broj koji se poziva na dugme CALL US
+
+$adresa[1]="Los Angeles"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[1]="Los Angeles ";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[1]="Los Angeles ";
+$telephone[1]=3233377008;// Broj koji se poziva na dugme CALL US
+
+$adresa[2]=""; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[2]="Miami ";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[2]="Miami";
+$telephone[2]=3059704521;// Broj koji se poziva na dugme CALL US
+
+$companynumber=3;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
